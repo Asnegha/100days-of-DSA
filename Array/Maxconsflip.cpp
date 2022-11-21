68caa0449b1bb925196c23644ee178954ee126ed
ASnegha <naicitamehtam555@gmail.com>
2022-11-21 12:25:34 +0530

    Max consective flip

---
+++ Array/Maxconsflip.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+void maxConsFlip(int a[], int n){
+
+    for(int i = 1; i < n; i++){
+       if(a[i] != a[i-1]){
+           if(a[i] != a[0]){
+            cout << "From " << i << " to ";
+           }else{
+            cout << i-1;
+           }
+       }
+
+    }
+        if(a[n-1] != a[0]){
+        cout << n-1;
+    }
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    maxConsFlip(a, n);
+}
